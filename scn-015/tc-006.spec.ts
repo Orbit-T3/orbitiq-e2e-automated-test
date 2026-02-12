cc4d9592c267ace892cd836312307467c4ed7d51
Denteen12330 <denteen12330@gmail.com>
2026-02-12 13:37:20 +0700

    test scn 015

---
+++ scn-015/tc-006.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://d3i0qiuq3enr0s.cloudfront.net/login');
+  await page.getByRole('button', { name: 'Sign in with Google' }).click();
+  await page.getByRole('textbox', { name: 'Email or phone' }).click();
+  await page.getByRole('textbox', { name: 'Email or phone' }).fill('');
+  await page.getByRole('button', { name: 'Next' }).click();
+  await page.getByRole('textbox', { name: 'Enter your password' }).fill('');
+  await page.getByRole('button', { name: 'Next' }).click();
+  await page.getByRole('button', { name: 'Open Menu' }).click();
+  await page.getByRole('link', { name: 'จัดการสาขา' }).click();
+  await page.getByRole('textbox', { name: 'ค้นหาสาขา' }).click();
+  await page.getByRole('textbox', { name: 'ค้นหาสาขา' }).fill('MXP-0005');
+  await page.screenshot({ path: 'results/SCN-015-TC-006.png', fullPage: true });
+});
